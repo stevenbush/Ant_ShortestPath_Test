1c1f8768e07600001a1173a239fbd7b77a80d8cb
shi_jiyuan <shi_jiyuan@outlook.com>
2014-08-26 09:44:48 +0800

    Submit Distribition_Test

---
+++ Test_Code/Distribution_Test.java
@@ -0,0 +1,16 @@
+import org.apache.commons.math3.distribution.EnumeratedIntegerDistribution;
+
+public class Distribution_Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] singletons = { 2, 4, 6, 8, 10 };
+		double[] probabilities = { 0.6, 0.1, 0.1, 0.1, 0.1 };
+		EnumeratedIntegerDistribution distribution = new EnumeratedIntegerDistribution(singletons, probabilities);
+		
+		for (int i = 0; i < 20; i++) {
+			System.out.println(distribution.sample());
+		}
+	}
+
+}
